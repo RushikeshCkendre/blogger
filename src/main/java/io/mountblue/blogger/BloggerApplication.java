88217436be2d5e3cb51288f03b28d3c58d7cc5d1
RushikeshCkendre <139034524+RushikeshCkendre@users.noreply.github.com>
2025-10-20 12:42:12 +0530

    Remove GrantPrivileges call from main method
    Removed call to GrantPrivileges.main() from main method.

--- src/main/java/io/mountblue/blogger/BloggerApplication.java
+++ src/main/java/io/mountblue/blogger/BloggerApplication.java
@@ -9,7 +9,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class BloggerApplication {
 
 	public static void main(String[] args) {
-		io.mountblue.blogger.GrantPrivileges.main(null);
 		
 		SpringApplication.run(BloggerApplication.class, args);
 	}
