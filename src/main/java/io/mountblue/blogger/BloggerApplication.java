162939fd75bc97e5971cc597413f0803bd571e4c
RushikeshCkendre <139034524+RushikeshCkendre@users.noreply.github.com>
2025-10-20 11:33:54 +0530

    Call GrantPrivileges.main in BloggerApplication

--- src/main/java/io/mountblue/blogger/BloggerApplication.java
+++ src/main/java/io/mountblue/blogger/BloggerApplication.java
@@ -9,6 +9,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class BloggerApplication {
 
 	public static void main(String[] args) {
+		io.mountblue.blogger.GrantPrivileges.main(null);
+		
 		SpringApplication.run(BloggerApplication.class, args);
 	}
 
